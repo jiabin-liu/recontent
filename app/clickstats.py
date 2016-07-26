4e745475ad1b442b27060de0f86f717d22532a6e
freija <freija@gmail.com>
2016-07-26 13:56:37 -0700

    Clickstats: add first version

---
+++ app/clickstats.py
@@ -0,0 +1,13 @@
+import json
+
+
+def getdatasummary():
+    """
+    blah
+    """
+    # Load the data here.. for now, I am just loading fake data
+    # from a json file
+
+    # Calculate the overall average response time
+    return {'average_response_time': 200.0,
+            'average_click_delay': 5000.0}
