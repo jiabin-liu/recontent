ea4d99903850adf5c4f55f9bcd4827c5c2ca1074
Jiabin Liu <jiabin@berkeley.edu>
2016-07-28 17:47:50 -0500

    Add files via upload

---
+++ tools/gensimple/get_arxiv_corpus.py
@@ -0,0 +1,44 @@
+# Freija Descamps <freija@gmail.com> July 2016
+# modified to download arxiv corpus
+
+import os
+import wget
+
+URL = 'https://dl.dropboxusercontent.com/u/99220436/recontent-data/arXiv1314/'
+
+CORPUS_NAME = 'arx'
+
+MMFILE = CORPUS_NAME + '.mm'
+DICTFILE = CORPUS_NAME + '_wordids.txt'
+SIMMATRIX = CORPUS_NAME + '-lsi.index'
+LSIMODEL = CORPUS_NAME + '.lsi_model'
+LSIPROJ = CORPUS_NAME + '.lsi_model.projection'
+ARTICLEDICT = CORPUS_NAME + '_adict.json'
+NPYINDEX = CORPUS_NAME + '-lsi.index.index.npy'
+
+DEFAULT_DICT_SIZE = 100000
+
+
+def main(argv=None):
+    # first clean up
+    try:
+        os.remove(MMFILE)
+        os.remove(DICTFILE)
+        os.remove(SIMMATRIX)
+        os.remove(LSIMODEL)
+        os.remove(ARTICLEDICT)
+        os.remove(LSIPROJ)
+        os.remove(NPYINDEX)
+    except OSError:
+        pass
+
+    wget.download(URL + MMFILE)
+    wget.download(URL + DICTFILE)
+    wget.download(URL + SIMMATRIX)
+    wget.download(URL + LSIMODEL)
+    wget.download(URL + ARTICLEDICT)
+    wget.download(URL + LSIPROJ)
+    wget.download(URL + NPYINDEX)
+
+if __name__ == "__main__":
+    main()
