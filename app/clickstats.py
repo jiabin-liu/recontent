8c2464c84d88ac42be15dc11cdfc99c4c393e602
freija <freija@gmail.com>
2016-07-27 18:17:22 -0700

    Add first real dashboard data and some website esthetics

--- app/clickstats.py
+++ app/clickstats.py
@@ -1,13 +1,36 @@
 import json
+import pymongo
+from pymongo import MongoClient
+
+client = MongoClient('db')
+db = client.clickdb
+clicks = db.clicks
 
 
 def getdatasummary():
     """
-    blah
+    This function returns some summary data from the click database.
     """
-    # Load the data here.. for now, I am just loading fake data
-    # from a json file
+    # First we will do some counting
+    # Get the total number of API requests = number of click documents
+    counter = clicks.count()
+    # Get the average response time of all time
+    cursor = clicks.find()
+    clickcount = 0
+    average_response_time = 0.0
+    average_click_delay = 0.0
+    for document in cursor:
+        time_of_request = document['time']
+        average_response_time += document['response_time']
+        # loop over all clicks inside the response document
+        for click in document['clicks']:
+            clickcount += 1
+            average_click_delay += click['time'] - time_of_request
+    average_click_delay = average_click_delay/clickcount
+    average_response_time = average_response_time/counter
 
     # Calculate the overall average response time
-    return {'average_response_time': 200.0,
-            'average_click_delay': 5000.0}
+    return {'counter': counter,
+            'average_response_time': average_response_time,
+            'average_click_delay': average_click_delay,
+            'clickcount': clickcount}
