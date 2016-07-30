fadf8fd4115b8be7ef4ed62deb3ee5f9a836ff51
freija <freija@gmail.com>
2016-07-30 13:15:00 -0700

    Final website tweaks

--- app/static/js/charts.js
+++ app/static/js/charts.js
@@ -7,15 +7,21 @@ function makeGraphs(error, dataJson) {
   'use strict';
   // Grab the data
 	var data = dataJson;
+
+
+
   // Now count the number of times a corpus was requested.
   var counts = [];
+  var responsetimes = [];
   data.forEach(function(d){
+    responsetimes.push(d["response_time"]);
     var corpusname = d["corpus"];
     if (typeof counts[corpusname] === 'undefined') {
       counts[corpusname] = 0;
     }
     counts[corpusname]++;
   })
+  //console.log(responsetimes);
   // The data now looks like this:
   // [speechwiki: 1, simple-wiki: 67, speech: 10]
   // We need to change it into a list of arrays of key value pairs
@@ -30,7 +36,7 @@ function makeGraphs(error, dataJson) {
   var donutWidth = 50;
   var legendRectSize = 18;
   var legendSpacing = 4;
-  var color = d3.scale.category10();
+  var color = d3.scale.category20b();
 
   var svg = d3.select('#piechart')
     .append('svg')
@@ -83,9 +89,9 @@ function makeGraphs(error, dataJson) {
 
   var tooltip = d3.select('#piechart')
     .append('div')
-    .attr('class', 'tooltip');
+    .attr('class', 'mytooltip');
   tooltip.append('div')
-    .attr('class', 'label');
+    .attr('class', 'label').html("test");
   tooltip.append('div')
     .attr('class', 'count');
   tooltip.append('div')
@@ -106,5 +112,61 @@ function makeGraphs(error, dataJson) {
   path.on('mouseout', function() {
     tooltip.style('display', 'none');
   });
+  // OPTIONAL
+  path.on('mousemove', function(d) {
+    tooltip.style('top', (d3.event.layerY + 10) + 'px')
+      .style('left', (d3.event.layerX + 10) + 'px');
+  });
+
+  // Plot the all-time response times
+  var data = responsetimes;
+  var formatCount = d3.format(",.0f");
+
+  var margin = {top: 10, right: 30, bottom: 30, left: 30},
+      width = 680 - margin.left - margin.right,
+      height = 300 - margin.top - margin.bottom;
+
+  var x = d3.scaleLinear()
+      .domain([0,5])
+      .rangeRound([0, width]);
+
+  var bins = d3.histogram()
+      .domain(x.domain())
+      .thresholds(x.ticks(50))
+      (data);
+
+  var y = d3.scaleLinear()
+      .domain([0, d3.max(bins, function(d) { return d.length; })])
+      .range([height, 0]);
+
+  var svg = d3.select("#hist").append('svg')
+      .attr("width", width + margin.left + margin.right)
+      .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+      .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+  var bar = svg.selectAll(".bar")
+      .data(bins)
+    .enter().append("g")
+      .attr("class", "bar")
+      .attr("transform", function(d) { return "translate(" + x(d.x0) + "," + y(d.length) + ")"; });
+
+  bar.append("rect")
+      .attr("x", 1)
+      .attr("width", x(bins[0].x1) - x(bins[0].x0) - 1)
+      .attr("height", function(d) { return height - y(d.length); });
+
+  bar.append("text")
+      .attr("dy", ".75em")
+      .attr("y", 6)
+      .attr("x", (x(bins[0].x1) - x(bins[0].x0)) / 2)
+      .attr("text-anchor", "middle")
+      .text(function(d) { return formatCount(d.length); });
+
+  svg.append("g")
+      .attr("class", "axis axis--x")
+      .attr("transform", "translate(0," + height + ")")
+      .call(d3.axisBottom(x));
+
 
 };
